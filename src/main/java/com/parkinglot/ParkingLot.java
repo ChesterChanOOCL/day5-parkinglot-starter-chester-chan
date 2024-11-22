d6633818324198ef21b3e905319a06be3627f744
CHANCH10 <chester.chan@oocl.com>
2024-11-22 14:12:44 +0800

    feat: check isFull() of parkingLot

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,15 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private Map<Ticket,Car> ticketToCar= new HashMap<>();
+    private int capacity ;
+    private Map<Ticket,Car> ticketToCar;
+    private int availablePosition;
+
+    public ParkingLot() {
+        this.capacity = 3; ;
+        this.availablePosition = 10;
+        this.ticketToCar = new HashMap<>();
+    }
+
     public Ticket park(Car car) {
+        if (!this.isFull()) {
         Ticket ticket = new Ticket();
         ticketToCar.put(ticket,car);
+        availablePosition -- ;
         return ticket;
+        }
+        return null;
     }
 
     public Car fetch(Ticket ticket) {
-        //write a map to map the car by ticket
-        return ticketToCar.get(ticket);
+        Car obtainedCar =  ticketToCar.get(ticket);
+        ticketToCar.remove(ticket);
+        return obtainedCar;
+    }
+    // the parking lot is full when the size of the map is 10
+    public boolean isFull() {
+        if (ticketToCar.size() >= capacity) {
+            return true;
+        }
+        return false;
     }
 }
