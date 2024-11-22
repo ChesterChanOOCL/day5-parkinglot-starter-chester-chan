14bd32c5985bf96acaa5750b1043b57c36baea2e
CHANCH10 <chester.chan@oocl.com>
2024-11-22 12:00:17 +0800

    test: fetchcar test

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -16,6 +17,19 @@ public class ParkingLotTest {
         assertNotNull(ticket);
 
     }
+    @Test
+    void should_return_the_car_when_fetch_given_a_ticket(){
+        // Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+
+        //When
+        Car fetchedCar = parkingLot.fetch(ticket);
+        //Return
+        assertEquals(car, fetchedCar);
+    }
+
 
 
 }
