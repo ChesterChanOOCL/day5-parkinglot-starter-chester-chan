908512e0eb44589427ee7b8829f7ac1598e5deb5
CHANCH10 <chester.chan@oocl.com>
2024-11-22 15:14:08 +0800

    feat:  return the ParkingLotFullException when parking a car given the car park is full

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -7,29 +7,31 @@ public class ParkingLot {
     private int capacity ;
     private Map<Ticket,Car> ticketToCar;
     private int availablePosition;
-
-    public ParkingLot() {
+     ParkingLot() {
         this.capacity = 3; ;
         this.availablePosition = 10;
         this.ticketToCar = new HashMap<>();
     }
 
+
+
     public Ticket park(Car car) {
         if (!this.isFull()) {
-        Ticket ticket = new Ticket();
-        ticketToCar.put(ticket,car);
-        availablePosition -- ;
-        return ticket;
+            Ticket ticket = new Ticket();
+            ticketToCar.put(ticket, car);
+            availablePosition--;
+            return ticket;
         }
-        return null;
+
+        throw new ParkingLotFullException("No available position");
     }
+
 
     public Car fetch(Ticket ticket) {
         Car obtainedCar =  ticketToCar.get(ticket);
         ticketToCar.remove(ticket);
         return obtainedCar;
     }
-    // the parking lot is full when the size of the map is 10
     public boolean isFull() {
         if (ticketToCar.size() >= capacity) {
             return true;
